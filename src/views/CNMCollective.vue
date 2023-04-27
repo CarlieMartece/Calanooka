9b5f192d5c82dde6ebf9205d479b1fa7df328eb3
CarlieMartece <carliemartece@gmail.com>
2023-04-27 21:08:39 +0100

    render alter names on CNMC page

--- src/views/CNMCollective.vue
+++ src/views/CNMCollective.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="cnmc">
-    <h1>CNMC</h1>
+  <div id="cnmc">
+    <h1>The Carlie Nooka Martece Collective</h1>
+    <p>(AKA When Autism Masking Got Out Of Hand)</p>
+    <ul>
+      <li v-for="alter in alters" :key="alter.id" :id="alter.name">
+        <h2>{{ alter.name }}</h2>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import sourceData from "../data/theCNMC.json";
+
+export default {
+  data() {
+    return {
+      alters: sourceData.alters,
+    };
+  },
+};
+console.log(sourceData.alters);
+</script>
