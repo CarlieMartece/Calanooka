2ffec9fa845d054d47b1f421c7963ec5c13c0c40
CarlieMartece <carliemartece@gmail.com>
2023-04-28 11:57:53 +0100

    update styling and render different sized CNMC headings depending on arcana

--- src/views/CNMCollective.vue
+++ src/views/CNMCollective.vue
@@ -1,13 +1,16 @@
 <template>
-  <div id="cnmc">
-    <h1>The Carlie Nooka Martece Collective</h1>
-    <p>(AKA When Autism Masking Got Out Of Hand)</p>
-    <ul>
-      <li v-for="alter in alters" :key="alter.id" :id="alter.name">
-        <h2>{{ alter.name }}</h2>
-      </li>
-    </ul>
-  </div>
+  <main>
+    <div id="cnmc">
+      <h1>The Carlie Nooka Martece Collective</h1>
+      <p>(AKA When Autism Masking Got Out Of Hand)</p>
+      <ul>
+        <li v-for="alter in alters" :key="alter.id" :id="alter.name">
+          <h2 v-if="alter.arcana==='Major'">{{ alter.name }}</h2>
+          <h3 v-else>{{ alter.name }}</h3>
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <script>
