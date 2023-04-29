829a2736dd599b3fee77ff58ffe70845bf84d74c
CarlieMartece <carliemartece@gmail.com>
2023-04-29 21:11:14 +0100

    move colours to variables and adjust styling

--- src/views/CNMCollective.vue
+++ src/views/CNMCollective.vue
@@ -1,15 +1,15 @@
 <template>
   <main>
-    <div id="cnmc">
+    <header>
       <h1>The Carlie Nooka Martece Collective</h1>
       <p>(AKA When Autism Masking Got Out Of Hand)</p>
-      <ul>
-        <li v-for="alter in alters" :key="alter.id" :id="alter.name">
-          <h2 v-if="alter.arcana==='Major'">{{ alter.name }}</h2>
-          <h3 v-else>{{ alter.name }}</h3>
-        </li>
-      </ul>
-    </div>
+    </header>
+    <ul id="cnmc">
+      <li v-for="alter in alters" :key="alter.id" :id="alter.nameId">
+        <h2 v-if="alter.arcana === 'Major'">{{ alter.name }}</h2>
+        <h3 v-else>{{ alter.name }}</h3>
+      </li>
+    </ul>
   </main>
 </template>
 
