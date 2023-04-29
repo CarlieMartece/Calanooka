59845fc5763c6251f9315c762d9f2037a9c5b739
CarlieMartece <carliemartece@gmail.com>
2023-04-29 22:29:05 +0100

    move alter names to card component and create css ids from name

---
+++ src/views/CNMAlterCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <li :id="nameToId">
+    <h2 v-if="arcana === 'Major'">{{ name }}</h2>
+    <h3 v-else>{{ name }}</h3>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    arcana: String,
+  },
+  computed: {
+    nameToId() {
+      const nameLower = this.name.toLowerCase();
+      const nameArray = nameLower.split(" ");
+      const nameId = nameArray.join("-");
+      return nameId;
+    },
+  },
+};
+</script>
--- src/views/CNMCollective.vue
+++ src/views/CNMCollective.vue
@@ -5,22 +5,28 @@
       <p>(AKA When Autism Masking Got Out Of Hand)</p>
     </header>
     <ul id="cnmc">
-      <li v-for="alter in alters" :key="alter.id" :id="alter.nameId">
-        <h2 v-if="alter.arcana === 'Major'">{{ alter.name }}</h2>
-        <h3 v-else>{{ alter.name }}</h3>
-      </li>
+      <AlterCard
+        v-for="alter in alters"
+        :key="alter.id"
+        :name="alter.name"
+        :arcana="alter.arcana"
+      />
     </ul>
   </main>
 </template>
 
 <script>
 import sourceData from "../data/theCNMC.json";
+import CNMAlterCard from "./CNMAlterCard.vue";
 
 export default {
   data() {
     return {
       alters: sourceData.alters,
     };
+  },
+  components: {
+    AlterCard: CNMAlterCard,
   },
 };
 console.log(sourceData.alters);
