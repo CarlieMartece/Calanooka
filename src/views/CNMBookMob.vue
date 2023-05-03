1e4ddb6c00e21d153cfceb0add7c0f8b5a6d6c96
CarlieMartece <carliemartece@gmail.com>
2023-05-03 11:03:01 +0100

    render first horizontal connection lines on CNMBookMob scss

--- src/views/CNMBookMob.vue
+++ src/views/CNMBookMob.vue
@@ -1,14 +1,21 @@
 <template>
   <h2>Book {{ bookNumToStr }}</h2>
-  <ul id="cnmcMob">
-    <AlterCard
-      v-for="alter in alters"
-      :key="alter.id"
-      :name="alter.name"
-      :arcana="alter.arcana"
-      :class="bookLower"
-    />
-  </ul>
+  <div id="cnmcMob">
+    <ul id="cnmcMobLinesAH">
+      <li v-for="line in linesXhered" :key="line.key" class="line-across-hereditary">
+        <div :id="line.id"></div>
+      </li>
+    </ul>
+    <ul id="cnmcMobCards">
+      <AlterCard
+        v-for="alter in alters"
+        :key="alter.id"
+        :name="alter.name"
+        :arcana="alter.arcana"
+        :class="bookLower"
+      />
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -22,6 +29,16 @@ export default {
   data() {
     return {
       alters: sourceData.alters,
+      linesXhered: [
+        { key: 1, id: "lah01" },
+        { key: 2, id: "lah02" },
+        { key: 3, id: "lah03" },
+        { key: 4, id: "lah04" },
+        { key: 5, id: "lah05" },
+        { key: 6, id: "lah06" },
+        { key: 7, id: "lah07" },
+        { key: 8, id: "lah08" },
+      ],
     };
   },
   components: {
